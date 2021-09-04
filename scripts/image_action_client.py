d0e197c285cfcace0877838b4074a3b601084c83
Gianluca Bardaro <gianluca.bardaro@gmail.com>
2021-09-04 10:53:10 +0200

    changed detect grasps service

--- scripts/image_action_client.py
+++ scripts/image_action_client.py
@@ -18,7 +18,7 @@ def image_action_client():
     client.wait_for_server()
 
     # Creates a goal to send to the action server.
-    goal = sciroc_msgs.msg.PerceptionGoal(mode=2)
+    goal = sciroc_msgs.msg.PerceptionGoal(mode=4)
 
     # Sends the goal to the action server.
     client.send_goal(goal)
